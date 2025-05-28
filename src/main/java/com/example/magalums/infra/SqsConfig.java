2d86f3ac461a1bb21628ddbb2c86320e04e27e0d
GBvaillant <vaillantgabriel@gmail.com>
2025-05-28 16:53:14 -0300

    commit changes

--- src/main/java/com/example/magalums/infra/SqsConfig.java
+++ src/main/java/com/example/magalums/infra/SqsConfig.java
@@ -7,6 +7,8 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 import java.net.URI;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 @Configuration
 public class SqsConfig {
@@ -14,12 +16,20 @@ public class SqsConfig {
     @Value("${aws.endpoint.localstack}")
     private String endpointLocalstack;
 
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
     public SqsClient sqsClient() {
         return SqsClient.builder()
                 .endpointOverride(URI.create(endpointLocalstack))
                 .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
-
 }
+
